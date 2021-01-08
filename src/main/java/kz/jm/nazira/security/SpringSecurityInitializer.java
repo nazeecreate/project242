ec7662533e16f0d2aa540b9ee320a998b2b00165
Nazee <is8universe@gmail.com>
2021-01-08 23:58:35 +0600

    SpringSecurityInitializer added

---
+++ src/main/java/kz/jm/nazira/security/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package kz.jm.nazira.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
